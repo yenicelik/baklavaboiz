9f7aa5b9b149d7d728d46d2d8eb978d1b7c5ff2b
yenicelik <segaslp@gmail.com>
2017-03-28 13:32:21 +0200

    Using external server

--- task1_ow9d8s/helper.py
+++ task1_ow9d8s/helper.py
@@ -14,13 +14,4 @@ def rms(pred, y):
     out = (1./pred.shape[0]) * np.sum(np.square(pred - y), axis=0)
     return out**0.5
 
-def rms_gradient(X, w, y):
-    return np.dot(X.transpose(), np.dot(X, w)) - np.dot(X.transpose(), y)
 
-def weight_update(X, w, y, mu, lr):
-
-    n = X.shape[0]
-
-    weight_term = (1- lr * mu) * w
-
-    return weight_term - lr/n * rms_gradient(X, w, y)
