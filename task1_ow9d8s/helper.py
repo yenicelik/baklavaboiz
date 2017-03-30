1ca85262f9a262a7ae81a67bfd15fdfa429f80d8
yenicelik <segaslp@gmail.com>
2017-03-30 11:14:19 +0200

    task1 try 1

--- task1_ow9d8s/helper.py
+++ task1_ow9d8s/helper.py
@@ -1,4 +1,12 @@
 import numpy as np
+import operator as op
+
+def ncr(n, r):
+    r = min(r, n-r)
+    if r == 0: return 1
+    numer = reduce(op.mul, xrange(n, n-r, -1))
+    denom = reduce(op.mul, xrange(1, r+1))
+    return numer//denom
 
 def get_train_cross_dataset(X_arr, y_arr, i):
     """ i must be the i'th iteration from k """
@@ -14,4 +22,67 @@ def rms(pred, y):
     out = (1./pred.shape[0]) * np.sum(np.square(pred - y), axis=0)
     return out**0.5
 
+
+def poly2d_kernel(X):
+    """ Will return a new matrix (with new shapes!) X_out that will include permutations of degree=degree as it's elements """
+    """ Let's keep this naive, and simple """
+    #columns number of the new X_out
+    m = X.shape[0]
+    n = X.shape[1]
+
+    new_cols = ncr(X.shape[1], 2) + ncr(X.shape[1], 1) + ncr(X.shape[1], 0)
+
+    X_out = np.zeros((X.shape[0], new_cols))
+
+    #Adding the one
+    X_out[np.arange(m), 0] = 1
+
+    #Adding the monomial terms
+    offset = ncr(X.shape[1], 0)
+    for c in range(X.shape[1]):
+        X_out[np.arange(m), offset + c] = X[np.arange(m), c] 
+
+    #Adding the binomial terms
+    offset = ncr(X.shape[1], 0) + ncr(X.shape[1], 1)
+    for c1 in range(X.shape[1]):
+        for c2 in range(c1):
+            X_out[np.arange(m), offset + c1 + c2] = X[np.arange(m), c1] * X[np.arange(m), c2] #should be elementwise multiplication
+
+    return X_out
+
+
+def poly3d_kernel(X):
+    """ Will return a new matrix (with new shapes!) X_out that will include permutations of degree=degree as it's elements """
+    """ Let's keep this naive, and simple """
+    #columns number of the new X_out
+    m = X.shape[0]
+    n = X.shape[1]
+
+    new_cols = ncr(X.shape[1], 3) + ncr(X.shape[1], 2) + ncr(X.shape[1], 1) + ncr(X.shape[1], 0)
+
+    X_out = np.zeros((X.shape[0], new_cols))
+
+    #Adding the one
+    X_out[np.arange(m), 0] = 1
+
+    #Adding the monomial terms
+    offset = ncr(X.shape[1], 0)
+    for c in range(X.shape[1]):
+        X_out[np.arange(m), offset + c] = X[np.arange(m), c] 
+
+    #Adding the binomial terms
+    offset = ncr(X.shape[1], 0) + ncr(X.shape[1], 1)
+    for c1 in range(X.shape[1]):
+        for c2 in range(c1):
+            X_out[np.arange(m), offset + c1 + c2] = X[np.arange(m), c1] * X[np.arange(m), c2] #should be elementwise multiplication
+
+
+    #Adding the binomial terms
+    offset = ncr(X.shape[1], 2) + ncr(X.shape[1], 1) + ncr(X.shape[1], 0)
+    for c1 in range(X.shape[1]):
+        for c2 in range(c1):
+            for c3 in range(c2):
+                X_out[np.arange(m), offset + c1 + c2 + c3] = X[np.arange(m), c1] * X[np.arange(m), c2] * X[np.arange(m), c3] #should be elementwise multiplication
+
+    return X_out
 
--- task1_ow9d8s/sgd.py
+++ task1_ow9d8s/sgd.py
@@ -4,32 +4,55 @@ def rms_gradient(X, w, y):
     return np.dot(X.transpose(), np.dot(X, w)) - np.dot(X.transpose(), y)
 
 def weight_update(X, w, y, mu, lr):
-
+    """ check the equations!!!! """
     n = X.shape[0]
-
     weight_term = (1- lr * mu) * w
-
     return weight_term - lr/n * rms_gradient(X, w, y)
 
-def run_sgd(X_train, y_train, X_cv, y_cv, w, mu, lr, fn_loss, MAX_STEPS=1000):
+
+def run_sgd(X_train, y_train, X_cv, y_cv, w, mu, lr, fn_loss, MAX_STEPS=1000, batch_size=100):
+    """ """
+
+    if batch_size > X_train.shape[0]:
+        print("batch size bigger than training data!!")
+
+    indices = np.arange(X_train.shape[0])
+    np.random.shuffle(indices)
+    indices = indices[:batch_size] #take random 'batch_size' data samples
 
     train_loss_list = []
     cv_loss_list = []
-
+    X = X_train[indices, :]
+    y = y_train[indices]
 
     for i in range(MAX_STEPS):
-        w = weight_update(X_train, w, y_train, mu, lr)
-        #The loss should decline.. which it does..
-        
-        ##Measure loss
-        train_predictions = np.dot(X_train, w)
-        train_loss = fn_loss(train_predictions, y_train)
-        train_loss_list.append(train_loss)
+        train_loss, cv_loss, w = sgd_step(X, y, X_cv, y_cv, w, mu, lr, fn_loss)
 
-               
-        cv_predictions = np.dot(X_cv, w)
-        cv_loss = fn_loss(cv_predictions, y_cv)
+        if(len(cv_loss_list) > 0 and cv_loss / cv_loss_list[0] > 100):
+            #print "Exploding!"
+            pass
+
+        train_loss_list.append(train_loss)
         cv_loss_list.append(cv_loss)
 
     return train_loss_list, cv_loss_list, w
+
+
+def sgd_step(X_train, y_train, X_cv, y_cv, w, mu, lr, fn_loss, get_loss=True):
+    #
+    w = weight_update(X_train, w, y_train, mu, lr)
+    
+    ##Measure loss
+    if get_loss:
+        train_predictions = np.dot(X_train, w)
+        train_loss = fn_loss(train_predictions, y_train)           
+        cv_predictions = np.dot(X_cv, w)
+        cv_loss = fn_loss(cv_predictions, y_cv)
+    
+    if get_loss:
+        return train_loss, cv_loss, w
+    else:
+        return [], [], w
+
+
        
